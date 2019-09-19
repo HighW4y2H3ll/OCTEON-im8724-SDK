1e415b610be4705529300682a0904e152f0abb15
HighW4y2H3ll <huzh@nyu.edu>
2019-09-19 18:04:33 +0000

    patch initramfs so we could boot to a usable system

--- linux/embedded_rootfs/pkg_makefiles/bison_host.mk
+++ linux/embedded_rootfs/pkg_makefiles/bison_host.mk
@@ -12,14 +12,14 @@ all: build install
 
 .PHONY: build
 build: ${DIR_HOST} ${DIR_HOST}/Makefile
-	${MAKE} -C ${DIR_HOST}
+	#${MAKE} -C ${DIR_HOST}
 
 ${DIR_HOST}/Makefile:
-	cd ${DIR_HOST} && PATH=${BUILD}/usr/bin:${PATH} CC=gcc CFLAGS= ./configure --quiet --prefix=${BUILD}/usr --host=i386
+	#cd ${DIR_HOST} && PATH=${BUILD}/usr/bin:${PATH} CC=gcc CFLAGS= ./configure --quiet --prefix=${BUILD}/usr --host=i386
 
 .PHONY: install
 install:
-	${MAKE} -j1 -C ${DIR_HOST} install
+	#${MAKE} -j1 -C ${DIR_HOST} install
 
 ${DIR_HOST}:
 	tar -zxf ${STORAGE}/${DIR}.tar.gz
